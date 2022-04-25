367d6878f8b2feb0a723f32c07b9e44e713b1e5d
Aiden Lin <98539623+AidenL173@users.noreply.github.com>
2022-04-25 02:30:19 +0000

    Completion

--- Rectangle.py
+++ Rectangle.py
@@ -1,9 +1,20 @@
 class Rectangle:
   def __init__(self, x, y, height, width):
+    '''
+    takes x, y coordinates, width, height and saves them as instance variables
+    x: int, desired x coordinates
+    y: int, desired y coordinates
+    height: int, desired height
+    width: int, desired width
+    '''
     self.x = x if x > 0 else 0
     self.y = y if y > 0 else 0
     self.height = height if height > 0 else 0
     self.width = width if width > 0 else 0
 
   def __str__(self):
+    '''
+    returns a string containing the x, y, width, and height of the rectangle
+    return: str, a string containing the x, y, width, and height of the rectangle
+    '''
     return "(x:" + str(self.x) + ", y:" + str(self.y) + ") width:" + str(self.width) + ", height:" + str(self.height)
--- Surface.py
+++ Surface.py
@@ -1,14 +1,21 @@
 from Rectangle import Rectangle
 
 class Surface:
-  def __init__(self, filename, x, y, h, w):
+  def __init__(self, filename, x, y, height, width):
+    '''
+    takes a filename string and saves it to the self.image. Takes x, y coordinates, width, height and creates a rectangle object stored in self.rect
+    filename: str, 
+    x: int, desired x coordinates
+    y: int, desired y coordinates
+    height: int, desired height
+    width: int, desired width
+    '''
     self.image = filename
-    self.rect = Rectangle(x, y, h, w)
+    self.rect = Rectangle(x, y, height, width)
 
   def getRect(self):
     '''
     returns the internal rectangle object
-    self: str, 
-    return: str, 
+    return: Rectangle.Rectangle, the created rectangle object that is stored in self.rect
     '''
     return self.rect
